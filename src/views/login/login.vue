f15c2a3a47ad1e65a72d0c18d622ebe5fbe55846
anhaixu <2745744569@qq.com>
2020-12-15 16:55:05 +0800

    12-15

---
+++ src/views/login/login.vue
@@ -0,0 +1,93 @@
+<template>
+  <div class="card">
+    <a-card title="请先登录" style="width: 500px">
+      <a-form layout="horizontal" :form="form" @submit="handleSubmit">
+        <a-form-item
+          :validate-status="userNameError() ? 'error' : ''"
+          :help="userNameError() || ''"
+        >
+          <a-input
+            v-decorator="[
+              'userName',
+              { rules: [{ required: true, message: 'Please input your username!' }] },
+            ]"
+            placeholder="Username"
+          >
+            <a-icon slot="prefix" type="user" style="color: rgba(0, 0, 0, 0.25)" />
+          </a-input>
+        </a-form-item>
+        <a-form-item
+          :validate-status="passwordError() ? 'error' : ''"
+          :help="passwordError() || ''"
+        >
+          <a-input-password
+            v-decorator="[
+              'password',
+              { rules: [{ required: true, message: 'Please input your Password!' }] },
+            ]"
+            type="password"
+            placeholder="Password"
+          >
+            <a-icon slot="prefix" type="lock" style="color: rgba(0, 0, 0, 0.25)" />
+          </a-input-password>
+        </a-form-item>
+        <a-form-item>
+          <a-button
+            type="primary"
+            html-type="submit"
+            :disabled="hasErrors(form.getFieldsError())"
+          >
+            登录
+          </a-button>
+        </a-form-item>
+      </a-form>
+    </a-card>
+  </div>
+</template>
+<script>
+function hasErrors(fieldsError) {
+  return Object.keys(fieldsError).some((field) => fieldsError[field]);
+}
+export default {
+  data() {
+    return {
+      hasErrors,
+      form: this.$form.createForm(this, { name: "horizontal_login" }),
+    };
+  },
+  mounted() {
+    this.$nextTick(() => {
+      // To disabled submit button at the beginning.
+      this.form.validateFields();
+    });
+  },
+  methods: {
+    // Only show error after a field is touched.
+    userNameError() {
+      const { getFieldError, isFieldTouched } = this.form;
+      return isFieldTouched("userName") && getFieldError("userName");
+    },
+    // Only show error after a field is touched.
+    passwordError() {
+      const { getFieldError, isFieldTouched } = this.form;
+      return isFieldTouched("password") && getFieldError("password");
+    },
+    handleSubmit(e) {
+      e.preventDefault();
+      this.form.validateFields((err, values) => {
+        if (!err) {
+          console.log("Received values of form: ", values);
+        }
+      });
+    },
+  },
+};
+</script>
+<style lang="less" scoped>
+.card {
+  height: 400px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
